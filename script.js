c4c7f3ec7529b192de0874c55503a791e26cc63f
souravsaraf2000 <souravsaraf2000@rediffmail.com>
2021-05-16 19:41:37 +0530

    JavaScript File for Calculator

--- script.js
+++ script.js
@@ -1,78 +1,29 @@
-var oneBtn = document.getElementById("one");
-var twoBtn = document.getElementById("two");
-var threeBtn = document.getElementById("three");
-var fourBtn = document.getElementById("four");
-var fiveBtn = document.getElementById("five");
-var sixBtn = document.getElementById("six");
-var sevenBtn = document.getElementById("seven");
-var eightBtn = document.getElementById("eight");
-var nineBtn = document.getElementById("nine");
-var plusBtn = document.getElementById("plus");
-var minusBtn = document.getElementById("minus");
-var intoBtn = document.getElementById("into");
-var divisionBtn = document.getElementById("division");
-var equalsBtn = document.getElementById("equals");
-var zeroBtn = document.getElementById("zero");
-var clearBtn = document.getElementById("clear");
-var inputBox = document.getElementById("input-box");
-var y;
-oneBtn.addEventListener("click",function(){
-    inputBox.value+=1;
-})
-twoBtn.addEventListener("click",function(){
-    inputBox.value+=2;
-})
-threeBtn.addEventListener("click",function(){
-    inputBox.value+=3;
-})
-fourBtn.addEventListener("click",function(){
-    inputBox.value+=4;
-})
-fiveBtn.addEventListener("click",function(){
-    inputBox.value+=5;
-})
-sixBtn.addEventListener("click",function(){
-    inputBox.value+=6;
-})
-sevenBtn.addEventListener("click",function(){
-    inputBox.value+=7;
-})
-eightBtn.addEventListener("click",function(){
-    inputBox.value+=8;
-})
-nineBtn.addEventListener("click",function(){
-    inputBox.value+=9;
-})
-zeroBtn.addEventListener("click",function(){
-    inputBox.value+=0;
-})
-plusBtn.addEventListener("click",function(){
-    inputBox.value+="+";
-})
-minusBtn.addEventListener("click",function(){
-    inputBox.value+="-";
-})
-intoBtn.addEventListener("click",function(){
-    inputBox.value+="*";
-})
-divisionBtn.addEventListener("click",function(){
-    inputBox.value+="/";
-})
-clearBtn.addEventListener("click",function(){
-    inputBox.value="";
-})
-console.log(y)
-equalsBtn.addEventListener("click",function(){
-    calc();
-})
-document.body.addEventListener('keydown',function(e){
-    if(e.key==="Enter")
+let buttons = document.querySelectorAll("div.btn");
+let elements = ["7","8","9","+","4","5","6","-","1","2","3","*","","0","=","/"];
+let inputBox = document.getElementById("input-box");
+for(let i=0;i<buttons.length-1;i++)
+{
+    if(i===12)
     {
-        calc();
+        buttons[i].addEventListener("click",function(){
+            inputBox.value="";
+        })
     }
-})
+    else if(i===14)
+    {
+        buttons[i].addEventListener("click",function(){
+            calc();
+        })
+    }
+    else
+    {
+        buttons[i].addEventListener("click",function(){
+            inputBox.value+=elements[i];
+        })
+    }
+}
 function calc(){
-    var x = inputBox.value;
+    let x = inputBox.value;
     y = eval(x);
     if(y===undefined){
         alert("Enter a valid expression");
@@ -80,3 +31,9 @@ function calc(){
     else
         inputBox.value = y;
 }
+document.body.addEventListener('keydown',function(e){
+    if(e.key==="Enter")
+    {
+        calc();
+    }
+})
